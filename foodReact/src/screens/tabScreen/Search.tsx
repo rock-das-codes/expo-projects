9fc5a5f089e7aa89afb8ac1c9fd8986adc4b233d
rock-das-codes <shoubhikdas4@gmail.com>
2026-05-22 10:51:34 +0530

    tabs done

---
+++ foodReact/src/screens/tabScreen/Search.tsx
@@ -0,0 +1,12 @@
+//search restaurant page
+import { View, Text } from 'react-native'
+import React from 'react'   
+const Search = () => {
+    return (    
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Search Screen</Text>
+        </View>
+    )
+}   
+
+export default React.memo(Search);
